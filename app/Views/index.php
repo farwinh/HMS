fd11c1552ed4587c2ba73a4f5d7401b0547b2128
muhannadhmnh <mrmuhannadhrf@gmail.com>
2022-06-28 19:54:15 +0530

    changed

--- app/Views/index.php
+++ app/Views/index.php
@@ -50,7 +50,15 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
-
+        <li class="nav-item active">
+          <a class="nav-link nlink" href="#">HOME <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nlink" href="#">GALLERY</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nlink" href="#">CONTACT US</a>
+        </li>
       </ul>
       <form class="form-inline my-2 my-lg-0">
         
